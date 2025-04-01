fdfe9454a9e2ff9367404338c644d2181beaf240
Aaron <164956740+aaronlongmiis@users.noreply.github.com>
2025-04-01 17:40:35 -0500

    Added movement collision
    finished video 3

--- Assets/Scripts/Player (Aaron)/PlayerControllerA.cs
+++ Assets/Scripts/Player (Aaron)/PlayerControllerA.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerControllerA : MonoBehaviour
 {
     public float moveSpeed;
+    public LayerMask solidObjectsLayer;
 
     private bool isMoving;
     private Vector2 input;
@@ -37,7 +38,8 @@ public class PlayerControllerA : MonoBehaviour
                 targetPos.x += input.x;
                 targetPos.y += input.y;
 
-                StartCoroutine(Move(targetPos));
+                if (IsWalkable(targetPos))
+                    StartCoroutine(Move(targetPos));
             }
         }
 
@@ -56,5 +58,14 @@ public class PlayerControllerA : MonoBehaviour
         transform.position = targetPos;
 
         isMoving = false;
+    }
+    private bool IsWalkable(Vector3 targetPos)
+    {
+        if (Physics2D.OverlapCircle(targetPos, 0.3f, solidObjectsLayer) != null)
+        {
+            return false;
+        }
+
+        return true;
     }
 }
